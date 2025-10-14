8367d02213089c3b297f070970125627dbf3fc5d
Louis-Félix <lfveillette@outlook.com>
2025-10-14 19:48:07 -0400

    rip

--- 1/cleaner.bash
+++ 1/cleaner.bash
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 fichier="customer_profiles.csv"
-IFS=$'\n'  
 oldIFS=$IFS
 ligne_parcouru=0
 
-for ligne in $(<$fichier)
+cat $fichier | while read ligne 
 do
     if [ "$ligne_parcouru" -eq 0 ]; then
         ((ligne_parcouru++))
         continue
     else
-        readarray -d , -t line_array <<< "$line"
-        echo $readarray
+        IFS="," read -ra line_array <<< "$ligne"
         for (( i=0; i < ${#line_array[@]}; i++))
         do
-            if [ -z "${line_array[i]}"]; then
-                echo $ligne
+            echo "${line_array[i]}"
+            if [ -z "${line_array[i]}" ]; then
+                echo "$ligne"
                 sed "${ligne_parcouru}d"
             fi
         done
